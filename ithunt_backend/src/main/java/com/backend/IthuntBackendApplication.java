b82c68f02e6812c78982fa5c9bf2fa9e71417f36
NgaikaX <157043402+NgaikaX@users.noreply.github.com>
2024-07-19 22:59:03 +0100

    "Connect backend to database"

---
+++ ithunt_backend/src/main/java/com/backend/IthuntBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthuntBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IthuntBackendApplication.class, args);
+	}
+
+}
